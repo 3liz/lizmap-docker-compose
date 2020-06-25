ecbd5dfbeb5803ecb201a6d9ff20915f91808f08
David Marteau <dmarteau@3liz.com>
2020-06-25 11:37:48 +0200

    Fix processing test plugin

---
+++ lizmap/processing/algorithms/__init__.py
@@ -0,0 +1,13 @@
+
+from .TestAlgorithmProvider import  TestAlgorithmProvider
+
+class Test:
+    def __init__(self):
+        pass
+
+
+def WPSClassFactory( iface: 'WPSServerInterface' ) -> Test:
+
+    iface.registerProvider( TestAlgorithmProvider() )
+    return Test()
+
