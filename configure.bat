86414c0a1097c002211786a55a35809c49c2c798
rmartin <rmartin@3liz.com>
2023-04-25 11:16:09 +0200

    .bat script to set-up conf as configure.sh

---
+++ configure.bat
@@ -0,0 +1,15 @@
+@echo off
+
+:: define some variable
+set SCRIPTDIR=%~dp0
+set INSTALL_DEST=%SCRIPTDIR%lizmap
+set INSTALL_SOURCE=%SCRIPTDIR%
+:: ensure it match with .env.windows !
+set QGIS_VERSION_TAG=3.28
+
+:: docker run that launch _configure (create service file/lizmapprofile, install plugin, ...)
+docker run -it -u 1000:1000 --rm -e INSTALL_SOURCE=/install -e INSTALL_DEST=/lizmap -e "LIZMAP_DIR=%INSTALL_DEST%" -e QGSRV_SERVER_PLUGINPATH=/lizmap/plugins -v "%INSTALL_SOURCE%:/install" -v "%INSTALL_DEST%:/lizmap" -v "%INSTALL_SOURCE%:/src" --entrypoint /src/configure.sh 3liz/qgis-map-server:%QGIS_VERSION_TAG% _configure
+
+:: all ok, next step is to launch docker-compose
+echo setup finished, you can run 'docker-compose --env-file=.env.windows up'
+ 
